4aeb7d2e868f75a8ea396ef4ff71c970ea28b955
Eva Sibinga <evasibinga@MacBook-Pro-5.local>
2021-05-09 20:21:46 -0400

    initial modal methods and documentation of http requests we're not doing

--- src/components/CaseModal.vue
+++ src/components/CaseModal.vue
@@ -1,20 +1,208 @@
 <template>
   <div id="container">
     <div id="modal-container">
-      <div class="content"></div>
+      <div class="content">
+        <div class="modal-top">
+          <div class="modal-intro">In this case, speech was...</div>
+          <div class="modal-protected">
+            {{ protectedSpeech() }}
+          </div>
+          <div class="modal-verdict">
+            {{ verdict() }}
+          </div>
+          <div class="modal-caseName">
+            {{ caseName() }}
+          </div>
+          <div class="modal-decision">
+            <!-- v:if "landmark = Yes" -->
+            {{ decision() }}
+          </div>
+        </div>
+        <div class="modal-before">
+          <div class="modal-keyIssue">
+            <!-- v:if "landmark = Yes" -->
+            {{ keyIssue() }}
+          </div>
+        </div>
+        <div class="modal-during">
+          <div class="modal-cleanSyl">
+            <!-- v:if "landmark = No" -->
+            {{ cleanSyl() }}
+          </div>
+        </div>
+        <div class="modal-after">
+          <div class="modal-significance">
+            <!-- v:if "landmark = Yes" -->
+            {{ significance() }}
+          </div>
+        </div>
+        <div class="modal-related-cases">
+          <div class="modal-related">
+            <!-- v:if "landmark = Yes" -->
+            {{ related() }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
-// import * as d3 from "d3";
+import * as d3 from "d3";
 
 export default {
   name: "CaseModal",
   data() {
-    return {};
+    return { title: "caseName", cases: [], topicSubset: {} };
+  },
+  methods: {
+    protectedSpeech: function () {
+      //  console.log("protex", this.cases[0].protected);
+      d3.select(".modal-protected")
+        .data(this.cases) // are we going to pass in the data for each case...? pre-load all modals?
+        .text(function (d) {
+          if (d.protected == "No") {
+            return "Not protected!";
+          } else if (d.protected == "Yes") {
+            return "Protected!";
+          }
+        })
+        .style("color", "#C33C05");
+    },
+    verdict: function () {
+      d3.select(".modal-verdict")
+        .data(this.cases) // are we going to pass in the data for each case...? pre-load all modals?
+        .text(function (d) {
+          return d.majVotes + " - " + d.minVotes;
+        })
+        .style("color", "black");
+    },
+    decision: function () {
+      // waiting for landmarks JSON
+      /** 
+      d3.select(".modal-decision")
+        .data(this.landmarks) // json dataset
+        .text(function (d) {
+          return d.decision;
+        })
+        .style("color", "black")
+        .style("font-family", "Noto Sans")
+        .style("font-size", ".75em");
+    },
+    */
+    },
+    caseName: function () {
+      d3.select(".modal-caseName")
+        .data(this.cases) // are we going to pass in the data for each case...? pre-load all modals?
+        .text(function (d) {
+          return d.caseName;
+        })
+        .style("color", "black");
+    },
+    cleanSyl: function () {
+      d3.select(".modal-cleanSyl")
+        .data(this.cases) // are we going to pass in the data for each case...? pre-load all modals?
+        .text(function (d) {
+          return d.cleansyl;
+        })
+        .style("color", "black")
+        .style("font-family", "Noto Sans")
+        .style("font-size", ".75em");
+    },
+    keyIssue: function () {
+      // waiting for landmarks JSON
+      /** 
+      d3.select(".modal-keyIssue")
+        .data(this.landmarks) // json dataset
+        .text(function (d) {
+          return d.keyissue;
+        })
+        .style("color", "black")
+        .style("font-family", "Noto Sans")
+        .style("font-size", ".75em");
+       */
+    },
+    significance: function () {
+      // waiting for landmarks JSON
+      /** 
+      d3.select(".modal-significance")
+        .data(this.landmarks) // json dataset
+        .text(function (d) {
+          return d.significance;
+        })
+        .style("color", "black")
+        .style("font-family", "Noto Sans")
+        .style("font-size", ".75em");
+       */
+    },
+    related: function () {
+      // waiting for landmarks JSON
+      /** 
+      d3.select(".modal-related")
+        .data(this.landmarks) // json dataset
+        .text(function (d) {
+          return d.related;
+        })
+        .style("color", "black")
+        .style("font-family", "Noto Sans")
+        .style("font-size", ".75em");
+       */
+    },
+  },
+  created() {
+    // const request = require("request");
+
+    // const URL =
+    //   "https://raw.githubusercontent.com/freedom-of-speech-project/fos-data/joanne-data/tmpvfull-05-03_09-59.csv?token=ALBDZW54MMFW4L6GWOWO55DATB4D6";
+    // const TOKEN = "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";
+    // var options = {
+    //   url: URL,
+    //   headers: {
+    //     Authorization: "token " + TOKEN,
+    //   },
+    // };
+    // function callback(error, response, body) {
+    //   console.log(response.statusCode);
+    //   console.error(error);
+    //   console.log(body);
+    // }
+    // request(options, callback);
+
+    // const myHeaders = new Headers();
+    // myHeaders.append("Content-Type", "text/csv");
+    // myHeaders.append(
+    //   "Authorization",
+    //   "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"
+    // );
+    // return fetch(
+    //   "https://raw.githubusercontent.com/freedom-of-speech-project/fos-data/joanne-data/tmpvfull-05-03_09-59.csv?token=ALBDZW76SECVCOYONASUWXDATB4XG",
+    //   {
+    //     method: "GET",
+    //     headers: myHeaders,
+    //   }
+    // );
+
+    Promise.all([
+      d3.csv(
+        "https://raw.githubusercontent.com/freedom-of-speech-project/fos-data/joanne-data/tmpvfull-05-03_09-59.csv?token=ALBDZWZ2YGRZICYRWTEEDHTATB46Y",
+        d3.autoType
+      ),
+
+      d3.csv(
+        "https://raw.githubusercontent.com/freedom-of-speech-project/fos/draft-production/topicSubset2.csv",
+        d3.autoType
+      ),
+    ]).then(([caseData, subsetData]) => {
+      this.cases = caseData;
+      console.log("new cases: ", this.cases);
+      // 4 columns --> incl. top topic + value
+      this.topicSubset2 = subsetData;
+    });
   },
 };
 </script>
+
+
 <style scoped>
 #container {
   top: 0;
@@ -29,9 +217,9 @@ export default {
 #modal-container {
   width: 700px;
   height: 900px;
-  top: 0;
+  top: 20px;
   left: 0;
-  bottom: 0;
+  bottom: 10;
   right: 0;
   margin: auto;
   padding: 5px 50px 25px 50px;
@@ -46,6 +234,37 @@ export default {
   width: 699px;
   height: 699px;
   overflow-y: scroll;
-  padding-right: 50px;
+  /* padding-right: 50px; */
+  display: grid;
+  display: grid;
+  grid-template-rows: repeat(auto-fill);
+  row-gap: 2px;
+}
+.modal-intro {
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
+.modal-protected {
+  font-family: Fredericka the Great, serif;
+  font-size: 50px;
+  font-weight: 400;
+  color: "#C33C05";
+}
+.modal-verdict {
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
+.modal-caseName {
+  margin-top: 2%;
+  margin-bottom: 2%;
+  padding-left: 7%;
+  padding-right: 7%;
+}
+.modal-cleanSyl {
+  text-align: justify;
+  padding-left: 7%;
+  padding-right: 7%;
+  margin-top: 2%;
+  margin-bottom: 2%;
 }
 </style>
